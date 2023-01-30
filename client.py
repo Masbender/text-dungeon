310032c2f6c2d73fc3e27c7784e9a36274494f62
jager <mshalewski@tutanota.com>
2023-01-29 23:10:56 -0600

    Barebones working server and client programs

--- client.py
+++ client.py
@@ -1,1 +1,26 @@
+'''
+SIMPLE CONNECT: The disconnect function will only work locally as, in testing it didnt make sense to have the client determine
+when the server's tcp socket would close. 
+'''
 import socket
+
+ipAddr = input("Connect to what IP?: ")
+port = 1337
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((ipAddr, port))
+
+while(True):
+    try:
+        cmd = input("Send a Command: ")
+        client_socket.send(cmd.encode())
+        if(cmd == "close"):
+            client_socket.send(cmd.encode())
+            break
+        svrRAW = client_socket.recv(1024)
+        svrDATA = svrRAW.decode()
+        print(svrDATA)
+    except:
+        print("TCP Connection closed!")
+    
+client_socket.close()
--- server.py
+++ server.py
@@ -1,10 +1,28 @@
-import socketserver
+'''
+TO ADD: Selection system between local network and port-forwarded games using ngrok
 
-HOST = "127.0.0.1"
-PORT = 1337
+DISCUSSING POINTS: What data should be sent between client and server beside positional information?
 
-class makeServer(socketserver.BaseRequestHandler):
-    def handle(self):
-        
+'''
+import socket
 
+host = '0.0.0.0' #
+port = 1337
 
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((host, port))
+
+while True:
+    server_socket.listen(5)
+    client_socket, client_address = server_socket.accept()
+    print(f"Accepted connection from {client_address}")
+    client_socket.sendall(b"Hello, client!\n")
+    client_socket.sendall(b"For a list of commands, type [help]!")
+    clientRAW = client_socket.recv(1024)
+    clientCMD = clientRAW.decode()
+    if(clientCMD == 'help'):
+        client_socket.send(b"TEMPORARY HELP MESSAGE: FILL WITH LIST OF COMMANDS IN THE FUTURE")
+    else:
+        print("Sorry, please try again")
+
+server_socket.close()
