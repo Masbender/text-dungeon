f120a6d456ed4bbec7e1a4d0cce54419cbcca09d
Noah Dinan <noah.dinan@gmail.com>
2023-06-23 11:20:31 -0500

    Update linux install script and image and create build directory

--- build/install.sh
+++ build/install.sh
@@ -5,9 +5,9 @@ mkdir text-dungeon ; cd text-dungeon ; touch text-dungeon.desktop
 
 # download needed files
 echo Downloading executable...
-curl -#fL -o text-dungeon https://github.com/Masbender/text-dungeon/releases/download/v0.01/text-dungeon
+curl -#fL -o text-dungeon https://github.com/Masbender/text-dungeon/releases/download/v1.0/text-dungeon-linux
 echo Downloading icon...
-curl -#fL -o logo.jpg https://raw.githubusercontent.com/Masbender/text-dungeon/main/logo.jpg
+curl -#fLO https://raw.githubusercontent.com/Masbender/text-dungeon/main/build/icon.png
 
 # allow binary to launch
 sudo chmod +x text-dungeon text-dungeon.desktop
@@ -17,8 +17,8 @@ echo "" > text-dungeon.desktop
 echo [Desktop Entry] >> text-dungeon.desktop
 echo Name=Text Dungeon >> text-dungeon.desktop
 echo Type=Application >> text-dungeon.desktop
-echo Comment=A cool text dungeon game in your terminal! >> text-dungeon.desktop
-echo Icon=$(pwd)/logo.jpg >> text-dungeon.desktop
+echo Comment=suffer alone in your terminal! >> text-dungeon.desktop
+echo Icon=$(pwd)/icon.png >> text-dungeon.desktop
 echo Exec=$(pwd)/text-dungeon >> text-dungeon.desktop
 echo Terminal=true >> text-dungeon.desktop
 
